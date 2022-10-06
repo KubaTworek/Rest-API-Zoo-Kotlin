39dced0222c3a427c22ff97c3daee55a52cb78c8
KubaTworek <kubatvorek@gmail.com>
2022-10-06 23:05:38 +0200

    initialize project

---
+++ src/main/kotlin/pl/jakubtworek/RestAPIZooKotlin/RestApiZooKotlinApplication.kt
@@ -0,0 +1,11 @@
+package pl.jakubtworek.RestAPIZooKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiZooKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApiZooKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/pl/jakubtworek/RestAPIZooKotlin/RestApiZooKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.jakubtworek.RestAPIZooKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiZooKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
